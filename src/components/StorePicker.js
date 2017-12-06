e6b8b22f134a86e39d914371d2531391da48e4d5
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2017-12-06 17:48:36 +0000

    Imported helper function for the Store Picker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// we use {} when we are importing specific exports that are not defaults
+import { getFunName } from '../helpers';
+
 class StorePicker extends React.Component {
 	render() {
 		return (
@@ -7,7 +10,8 @@ class StorePicker extends React.Component {
 			  { /* This is a Valid React JS Comment */}
 			  { /* Comments cannot be placed on the first line of a return statement */}
 			  <h2>Please Enter A Store</h2>
-			  <input type="text" required placeholder="Store Name" />
+			  { /* We can now call the imported getFunName */}
+			  <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
 			  <button type="submit">Visit Store</button>
 			</form>
 		);
