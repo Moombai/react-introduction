ee4a89e0389d8def62106376eedf98c6646553bd
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2017-12-07 10:37:15 +0000

    Working with React Events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,14 +4,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+	// // We can use the constructor to add new methods to store picker
+	// constructor() {
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this); // find method and bind to store picker
+	// }
+
+	goToStore(event) {
+		event.preventDefault(); // regular JavaScript
+		// first grab the text from the box
+		console.log(this.storeInput.value); // storeInput was added in the render method
+		// second transition from / to /store/:storeId
+	}
+
 	render() {
+		// Render is a method of React.Component and it's 'this' is already bound to Store Picker
 		return (
-			<form className="store-selector">
-			  { /* This is a Valid React JS Comment */}
-			  { /* Comments cannot be placed on the first line of a return statement */}
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 			  <h2>Please Enter A Store</h2>
 			  { /* We can now call the imported getFunName */}
-			  <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+			  <input type="text" required placeholder="Store Name" defaultValue={getFunName()}
+			  ref={(input) => { this.storeInput = input } }/>
+			  { /* the inline javascript on 'input' adds a reference directly to the class */}
 			  <button type="submit">Visit Store</button>
 			</form>
 		);
