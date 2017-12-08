6740f13d1792e0f8c677065d150d271274e84142
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2017-12-08 16:36:23 +0000

    Create a method to set the App state to that of the sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 	// We cannot use 'this' in a constructor until we call super(), which will initialise React.Component
@@ -11,6 +12,7 @@ class App extends React.Component {
 
 		// bind addFish method to App
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
 
 		// Initial state
 		this.state = {
@@ -30,6 +32,12 @@ class App extends React.Component {
 		this.setState({ fishes: fishes }); // alternative input: ({ fishes })
 	}
 
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -38,7 +46,7 @@ class App extends React.Component {
 				</div>
 				<Order />
 				{/* pass the add fish method down to inventory as a prop */}
-				<Inventory addFish={this.addFish} />
+				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 		);
 	}
