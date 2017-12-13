beb9a5457c91befd9ece7c39a898cc94360e2a1a
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2017-12-13 10:07:09 +0000

    Implment local storage for order state

--- src/components/App.js
+++ src/components/App.js
@@ -27,14 +27,32 @@ class App extends React.Component {
 	}
 
 	componentWillMount() {
+		// this runs right before the <App> is rendered
 		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
 			context: this,
 			state: 'fishes'
 		});
+
+		//check if there is any order in localStorage
+		const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+		if(localStorageRef) {
+			// update our App components order state
+			this.setState({
+				order: JSON.parse(localStorageRef)
+			})
+		}
 	}
 
 	componentWillUnmount() {
 		base.removeBinding(this.ref);
+	}
+
+	// Object state and props after  they've been updated
+	componentWillUpdate(nextProps, nextState) {
+		//local storage cannot accept objects so we store our value as a JSON string
+		localStorage.setItem(`order-${this.props.params.storeId}`,
+			JSON.stringify(nextState.order));
 	}
 
 	addFish(fish) {
@@ -78,7 +96,11 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order}/>
+				<Order
+					fishes={this.state.fishes}
+					order={this.state.order}
+					params={this.props.params}
+				/>
 				{/* pass the add fish method down to inventory as a prop */}
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
